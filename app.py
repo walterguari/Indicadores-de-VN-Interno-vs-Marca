c55aaeb4aeaae569c3fb2195067953f069dce754
walterguari <analista.autociel@gmail.com>
2026-07-26 12:04:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1135,8 +1135,7 @@ try:
                                 "No se proporciono ningun contacto valido",
                                 "Correo electrónico/teléfono ausente;Correo electrónico/teléfono Inválido",
                                 "Invalid Email",
-                                "Mandatory field missing - email; invalid email",
-                                "EventDateTooOld"
+                                "Mandatory field missing - email; invalid email"
                             ]
                             cant_rechazos = 0
                             if col_rechazo:
